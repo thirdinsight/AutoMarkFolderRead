8662f120251956b8fd895959c4d1d791b50c8662
root <silvester.ersek@web-window.net>
2022-07-27 21:16:59 +0200

    switch from storage.sync to storage.local

--- src/js/common.js
+++ src/js/common.js
@@ -10,14 +10,14 @@ function buildDefaultCommonFolders() {
 async function loadCommonFoldersOptions() {
   let commonFolders = buildDefaultCommonFolders();
 
-  let result = await messenger.storage.sync.get('commonFolders');
+  let result = await messenger.storage.local.get('commonFolders');
   
-	console.log("Result loaded from sync storage: " + JSON.stringify(result));
+	console.log("Result loaded from local storage: " + JSON.stringify(result));
 	
 	if (result.hasOwnProperty("commonFolders") && Object.keys(result.commonFolders).length > 0) {
 	  commonFolders = result.commonFolders
 	  
-	  console.log("Using common folders loaded from sync storage: " + JSON.stringify(commonFolders));
+	  console.log("Using common folders loaded from local storage: " + JSON.stringify(commonFolders));
 	}
 	else {
 	  console.log("Using default common folders: " + JSON.stringify(commonFolders));
